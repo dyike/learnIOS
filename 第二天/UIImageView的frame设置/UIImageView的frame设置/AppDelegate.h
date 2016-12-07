be1c20777941bb50f7ac67c5a446806495ab4fb7
ityike <yuanfeng634@gmail.com>
2016-12-07 09:20:17 +0800

    UIImageView frame的设置

---
+++ 第二天/UIImageView的frame设置/UIImageView的frame设置/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImageView的frame设置
+//
+//  Created by ityike on 2016/12/7.
+//  Copyright © 2016年 ityike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 第二天/UIImageView的frame设置/UIImageView的frame设置/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImageView的frame设置
+//
+//  Created by ityike on 2016/12/7.
+//  Copyright © 2016年 ityike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
