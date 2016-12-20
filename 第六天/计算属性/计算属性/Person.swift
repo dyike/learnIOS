ec967cd6250cbee2c9a54b04b14bdaff99898374
ityike <yuanfeng634@gmail.com>
2016-12-20 08:08:12 +0800

    计算型属性和懒加载的区别

--- 第六天/计算属性/计算属性/Person.swift
+++ 第六天/计算属性/计算属性/Person.swift
@@ -31,7 +31,22 @@ class Person: NSObject {
     }
     
     // 只读属性可以直接return（简写）
+    // 又称为计算型属性-> 本身不保存内容，都是通过计算获得结果
+    // 类似于一个函数，没有参数，但是一定有返回值
+    // 懒加载的属性会分配空间存储 
     var title2: String {
         return "Mr. XXX." + (name ?? "")
     }
+    
+    // 懒加载的title 本质上一个闭包
+    // 懒加载会在第一次访问的时候，执行，闭包执行完毕后，会把结果保存在title3 中
+    // 后续调用，直接返回title3 的内容
+    lazy var title3: String = {
+        return "Lazy" + (self.name ?? "")
+    }()
+    
+    
+    
+    
+    
 }
